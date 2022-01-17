796d995aae1183679613b48d406888f7148cdfa9
Kamaria Hardy <kamehardy@gmail.com>
2022-01-17 16:38:22 -0500

    Add files via upload

---
+++ Day 2/day2.py
@@ -0,0 +1,30 @@
+# Numbers practice
+
+add = 12 + 31
+multi = 24 * 32
+div = 24 / 432
+sub = 31 - 21
+
+print(add)
+print(multi)
+print(div)
+print(sub)
+
+# String practice
+str = "Hello World"
+print(str)
+# Single or double quotes doesn't matter still a string
+str_single = 'Hola Amigos'
+print(str_single)
+# Strings can be multiplied
+str_multiply = str * 3
+print(str_multiply)
+# strings can be added to each other - concatenation
+str_add = str + str_single
+print(str_add)
+# to use quotes in the string use an escape character
+str_esc = "Hello World, this is \"awesome\"!"
+print(str_esc)
+# can add line breaks to a string
+str_lb = "Hello\nWorld"
+print(str_lb)
