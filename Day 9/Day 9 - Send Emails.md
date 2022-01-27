8439c694dcb8af04641f3865f5f548412abb3754
Kamaria Hardy <kamehardy@gmail.com>
2022-01-27 14:23:38 -0500

    Add files via upload

---
+++ Day 9/Day 9 - Send Emails.md
@@ -0,0 +1,8 @@
+# Day 9 - Send Emails and Read Inbox
+
+For this day I combined lessons 7-9 from Coding for Entrepreneurs' 30 days of Python course. In these lessons, I created a function to format a message that I could import as an internal module into other script, from there I created three additional scripts.
+- send - that sends messages to a list of to emails
+- send_mail - creates the server to send emails
+- inbox - allows you to read your inbox for unseen messages
+
+Together these scripts allow you to send messages to a less secured gmail account and read messages from that same account.
---
+++ Day 9/send_mail.py
@@ -0,0 +1,33 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+username = 'kamtestpy@gmail.com'
+password = 'Test4UGrrl!'
+
+def send_mail(text='Email Body', subject='Hello World', from_email=' Kam Test <kamtestpy@gmail.com>', to_emails=None, html=None):
+    assert isinstance(to_emails, list) # raises error if not a list
+    msg = MIMEMultipart('alternative')
+    msg['From'] = from_email
+    msg['To'] = ", ".join(to_emails)
+    msg['Subject'] = subject
+
+    txt_part = MIMEText(text, 'plain')
+    msg.attach(txt_part)
+    if html != None:
+        html_part = MIMEText(html, 'html')
+        msg.attach(html_part)
+
+
+    msg_str = msg.as_string()
+    # login to smtp server
+    server = smtplib.SMTP(host='smtp.gmail.com', port=587)
+    server.ehlo()
+    server.starttls()
+    server.login(username, password)
+    server.sendmail(from_email, to_emails, msg_str)
+
+    server.quit()
+
+
+        
